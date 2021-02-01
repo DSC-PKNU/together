a5406855670aef6524909538b0833c21ebacf22a
emptyshores <73519714+emptyshores@users.noreply.github.com>
2021-02-01 21:16:11 +0900

    Add files via upload

--- Together/app/src/main/java/com/example/together/EmergencyCallSetting.kt
+++ Together/app/src/main/java/com/example/together/EmergencyCallSetting.kt
@@ -1,10 +1,19 @@
 package com.example.together
 
 import android.app.Activity
+<<<<<<< HEAD
+import android.content.ContentUris
+import android.content.Intent
+import android.content.Context
+import android.content.ContentResolver
+import android.database.Cursor
+import androidx.core.content.ContextCompat
+=======
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.content.ContentResolver
 import android.database.Cursor
+>>>>>>> 139a709877aedb50a6da02837afb536f85867e1b
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.view.LayoutInflater
@@ -16,12 +25,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.emergency_call_list.view.*
 import java.util.*
+<<<<<<< HEAD
+
+class EmergencyCallSetting : Fragment() {
+
+    var data= arrayListOf<Contact>(
+=======
 
 class EmergencyCallSetting : Fragment() {
 
 
 
     val data = arrayListOf(
+>>>>>>> 139a709877aedb50a6da02837afb536f85867e1b
             Contact(1,
                     "엄마",
                     "01011112222",
@@ -34,7 +50,12 @@ class EmergencyCallSetting : Fragment() {
                     "동생",
                     "01011112224",
                     R.drawable.ic_location)
+<<<<<<< HEAD
+    )
+
+=======
     );
+>>>>>>> 139a709877aedb50a6da02837afb536f85867e1b
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreate(savedInstanceState)
@@ -53,6 +74,35 @@ class EmergencyCallSetting : Fragment() {
                 super.onActivityResult(requestCode, resultCode, data)
                 if (resultCode == Activity.RESULT_OK) {
 
+<<<<<<< HEAD
+                    val context=this.getContext()
+                    if (context!= null) {
+                    val contentResolver: ContentResolver = context.contentResolver
+
+                    val listUrl = ContactsContract.Contacts.CONTENT_URI
+                    val proj = arrayOf(
+                            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                            ContactsContract.CommonDataKinds.Phone.NUMBER,
+                            ContactsContract.CommonDataKinds.Phone.PHOTO_ID
+                    )
+
+
+                        val cursor = contentResolver.query(listUrl, proj, null, null, null)
+
+                        if (cursor != null) {
+
+                            cursor.moveToFirst();
+                            var Name = cursor.getString(0)
+                            var Phone = cursor.getString(1)
+                            var Image = cursor.getInt(2)
+
+                            cursor.close()
+
+                            (data as MutableList<Contact>).add(Contact(1, Name, Phone, Image))
+                        }
+
+                    }
+=======
                     val cursor: Cursor = ContentResolver.query(
                             arrayOf(
                                     ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
@@ -67,12 +117,17 @@ class EmergencyCallSetting : Fragment() {
                     //data.
                     cursor.close();
 
+>>>>>>> 139a709877aedb50a6da02837afb536f85867e1b
                 }
 
             }
             getActivity()?.finish();
         }
 
+<<<<<<< HEAD
+
+=======
+>>>>>>> 139a709877aedb50a6da02837afb536f85867e1b
         return view;
     }
 }
